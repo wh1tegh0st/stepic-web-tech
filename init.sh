187a1477aa1e6bf9dcb54d6ddc04a0323ea535c1
me <me@localhost>
2020-10-24 13:52:00 +0500

    Use newer application versions

--- init.sh
+++ init.sh
@@ -3,3 +3,8 @@
 sudo rm -f /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+
+# Newer than the system gunicorn
+cd web || exit 1
+. .venv/bin/activate
+gunicorn -c etc/gunicorn.config.py hello:app &
---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The environment is too old, update it
+sudo apt-get update
+sudo apt-get install -y python3.5
+sudo apt-get install -y python3.5-dev
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install --upgrade pip
+
+virtualenv -p python3 .venv
+. .venv/bin/activate
+
+pip install --upgrade gunicorn
+
+deactivate
